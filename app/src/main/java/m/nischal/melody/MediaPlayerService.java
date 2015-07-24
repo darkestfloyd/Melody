c32e032431975127b66a31a281bfa7c3af96e1f8
Nischal M <nischal.bhatewara@gmail.com>
2015-07-24 12:19:43 +0530

    Working notification action

--- app/src/main/java/m/nischal/melody/MediaPlayerService.java
+++ app/src/main/java/m/nischal/melody/MediaPlayerService.java
@@ -14,7 +14,6 @@ import android.widget.RemoteViews;
 
 import java.io.IOException;
 
-import m.nischal.melody.ui.MainActivity;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
@@ -25,11 +24,15 @@ import static m.nischal.melody.Helper.GeneralHelpers.DebugHelper.LumberJack;
 
 public class MediaPlayerService extends Service implements MediaPlayer.OnCompletionListener {
 
-    public static final String INTENT_EXTRA = "m.nischal.melody.service_intent_extra";
-    public static final int INTENT_EXTRA_PLAY = 0;
+    /*public static final int INTENT_EXTRA_PLAY = 0;
     public static final int INTENT_EXTRA_PAUSE = 1;
     public static final int INTENT_EXTRA_NEXT = 2;
-    public static final int INTENT_EXTRA_PREV = 3;
+    public static final int INTENT_EXTRA_PREV = 3;*/
+
+    public static final String ACTION_PLAY = "m.nischal.melody.MediaPlayerService.PLAY";
+    public static final String ACTION_PAUSE = "m.nischal.melody.MediaPlayerService.PAUSE";
+    public static final String ACTION_NEXT = "m.nischal.melody.MediaPlayerService.NEXT";
+    public static final String ACTION_PREV = "m.nischal.melody.MediaPlayerService.PREV";
 
     private final static MediaPlayer mPlayer = new MediaPlayer();
     private static boolean foreground = false;
@@ -39,7 +42,7 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
     private NotificationCompat.Builder notificationBuilder;
 
     private Notification notification;
-
+    private RemoteViews remoteView;
     private final IMelodyPlayer.Stub mBinder = new IMelodyPlayer.Stub() {
 
         @Override
@@ -111,9 +114,10 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        int intentExtraValue = intent.getIntExtra(INTENT_EXTRA, -1);
-        if (intentExtraValue != -1) {
-            LumberJack.d("extra value: ", intentExtraValue);
+        String action = intent.getAction();
+
+        if (action != null) {
+            LumberJack.d("extra value: " + action);
         } else {
             mPlayer.setOnCompletionListener(this);
             setUpNotification();
@@ -123,27 +127,31 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
 
     private void setUpNotification() {
 
+        remoteView = new RemoteViews(getPackageName(), R.layout.notification_expanded);
+        remoteView.setInt(R.id.notificationView, "setBackgroundColor", getResources().getColor(R.color.primary));
+
+        Intent i1 = new Intent(this, MediaPlayerService.class);
+        i1.setAction(ACTION_PLAY);
+        PendingIntent p1 = PendingIntent.getService(this, 0x2, i1, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent i2 = new Intent(this, MediaPlayerService.class);
+        i2.setAction(ACTION_NEXT);
+        PendingIntent p2 = PendingIntent.getService(this, 0x2, i2, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent i3 = new Intent(this, MediaPlayerService.class);
+        i3.setAction(ACTION_PREV);
+        PendingIntent p3 = PendingIntent.getService(this, 0x2, i3, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        remoteView.setOnClickPendingIntent(R.id.action_next, p2);
+        remoteView.setOnClickPendingIntent(R.id.action_prev, p3);
+        remoteView.setOnClickPendingIntent(R.id.action_play_pause, p1);
+
         notificationBuilder =
                 new android.support.v7.app.NotificationCompat.Builder(this)
                         .setColor(getResources().getColor(R.color.primary))
-                        .setSmallIcon(R.mipmap.ic_launcher);
-
-        /*Intent i1 = new Intent(this, MediaPlayerService.class);
-        i1.putExtra(INTENT_EXTRA, 1);
-        PendingIntent p1 = PendingIntent.getService(this, 1, i1, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Intent i2 = new Intent(this, MediaPlayerService.class);
-        i2.putExtra(INTENT_EXTRA, 2);
-        PendingIntent p2 = PendingIntent.getService(this, 2, i2, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Intent i3 = new Intent(this, MediaPlayerService.class);
-        i3.putExtra(INTENT_EXTRA, 3);
-        PendingIntent p3 = PendingIntent.getService(this, 3, i3, PendingIntent.FLAG_UPDATE_CURRENT);*/
-
-        notificationBuilder
-                .setContentText("test")
-                .setContentTitle("title")
-                .setSmallIcon(R.mipmap.ic_launcher);
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentText("test")
+                        .setContentTitle("title");
     }
 
     @Override
@@ -155,10 +163,6 @@ public class MediaPlayerService extends Service implements MediaPlayer.OnComplet
     }
 
     private void makeForeground() {
-        RemoteViews remoteView = new RemoteViews(getPackageName(), R.layout.notification_expanded);
-        remoteView.setInt(R.id.notificationView, "setBackgroundColor", getResources().getColor(R.color.primary));
-//        remoteView.setOnClickPendingIntent(R.id.notificationAction, PendingIntent.getActivity(this, 1, new Intent(this, MainActivity.class), 0));
-
         notification = notificationBuilder
                 .setAutoCancel(false)
                 .build();
